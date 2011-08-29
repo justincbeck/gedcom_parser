d879a82f45158275c20130c944a0b42fe6c552a1
Justin Beck <justinbeck@mac.com>
2011-08-29 13:17:09 -0400

    Sorted out usage of the stack.  Still some issues with XML not being valid but will sort that out.

--- lib/gedcom_processor/parser.rb
+++ lib/gedcom_processor/parser.rb
@@ -3,7 +3,6 @@ include REXML
 
 module GedcomProcessor
   class Parser
-
     def initialize(output)
       @output = output
       @components = Array.new
@@ -11,57 +10,52 @@ module GedcomProcessor
     end
 
     def parse(in_file, out_file)
+      stack = Array.new
+      stack.push []
+      p_depth = 0
       document = Document.new
       root = Element.new "gedcom"
       document.add_element root
 
       in_file.each_line do |line|
-        segments = line.split
-        depth = segments.shift
-        e_name = segments.shift
-        e_data = segments.join " "
+        if line.strip.length > 0
+          segments = line.split
+          depth = segments.shift.to_i
+          e_name = segments.shift
+          e_data = segments.join " "
 
-#        puts "depth: " + depth
-#        puts "element: " + e_name
-#        puts "data: " + e_data
-
-        if e_data.eql? "INDI"
-          element = Element.new e_data.downcase
-          element.add_attribute "id", e_name
-        else
           element = Element.new e_name.downcase
           element.text = e_data
 
-          if e_name.eql? "NAME"
-            n_segs = e_data.split
+          if p_depth == depth
+            peers = stack.pop
+            peers << element
+            stack.push peers
+          elsif depth.to_i > p_depth.to_i
+            stack.push [element]
+          elsif depth.to_i < p_depth.to_i
+            dd = p_depth - depth
+            dd.times do
+              youngsters = stack.pop
+              peers = stack.pop
+              youngsters.each do |y|
+                peers.last.add_element y
+              end
 
-            s_el = Element.new "surn"
-            s_el.text = n_segs.pop.scan(/\w+/)[0]
-            element.add_element s_el
-
-            g_el = Element.new "givn"
-            g_el.text = n_segs.join " "
-            element.add_element g_el
+              peers << element
+              stack.push peers
+            end
           end
+
+          p_depth = depth
         end
-
-        document.root.add_element element
-
       end
-      document.write STDOUT, 2
-    end
 
-    private
-
-    def parse_component(component)
-      type = component[0][1]
-      case type
-        when "INDI"
-          c_parser = IndividualParser.new
-          c_parser.parse component
-        else
-          @output.puts "Not yet implemented"
+      stack.pop.each do |e|
+        document.root.add_element e
       end
+
+      document.write out_file, 2
     end
   end
 end
