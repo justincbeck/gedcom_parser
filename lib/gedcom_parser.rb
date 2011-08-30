dc328bca0f8b2db72b163b223a04665e9803a843
Justin Beck <justinbeck@mac.com>
2011-08-29 23:13:17 -0400

    Cleaned out some files I no longer need.  Refactored some stuff that used those old files.

--- lib/gedcom_parser.rb
+++
@@ -1,1 +0,0 @@
-# Do any necessary setup here
--- lib/gedcom_processor/processor.rb
+++ lib/gedcom_processor/processor.rb
@@ -7,44 +7,23 @@ module GedcomProcessor
     end
     
     def process(file_name)
-      open_in_file(file_name)
-      
-      if !@in_file.nil?
-        create_out_file(file_name)
-        
-        parser = Parser.new(@output)
-        parser.parse(@in_file, @out_file)
-        
-        close_in_file
-        close_out_file
-        
-        @output.puts "File successfully parsed."
-      end
-    end
-    
-    private
-
-    def open_in_file(file_name)
       begin
         @in_file = File.open(file_name)
-        @output.puts "File successfully opened."
       rescue Exception => e
         @output.puts e.to_s
       end
-    end
-    
-    def close_in_file
-      @in_file.close
-    end
-    
-    def create_out_file(file_name)
-      name = /^\.*\/?(?:\w+\/)*(?:\.{2}\/)?(?:\w+\/)*(\w+)\.\w{3}$/.match(file_name)[1]
-      out_file_name = File.join(File.dirname(__FILE__), '../../', 'output', name + ".xml")
-      @out_file = File.new(out_file_name, "w+")
-    end
-    
-    def close_out_file
-      @out_file.close
+
+      if !@in_file.nil?
+        name = /^\.*\/?(?:\w+\/)*(?:\.{2}\/)?(?:\w+\/)*(\w+)\.\w{3}$/.match(file_name)[1]
+        out_file_name = File.join(File.dirname(__FILE__), '../../', 'output', name + ".xml")
+        @out_file = File.new(out_file_name, "w+")
+
+        parser = Parser.new(@output)
+        parser.parse(@in_file, @out_file)
+        
+        @in_file.close
+        @out_file.close
+      end
     end
   end
 end
